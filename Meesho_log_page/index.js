27a21700600f84c0f7a6ccefe7965c4bfea7d256
OnlySarthak <devsarthak.hotline@gmail.com>
2024-06-10 20:39:17 +0530

    Add files via upload

---
+++ Meesho_log_page/index.js
@@ -0,0 +1,19 @@
+// console.log("Hello world..");
+//     // alert("kusch nahi...lage raho munna bhai");
+//     document.write("this is document write");
+//     console.warn("This is Warning");
+//     console.error("This is Error");
+
+// //data-types
+// var number1 = 34 ;
+// var number2 = 45 ;
+// console.log(number1+number2);
+
+// //dates in js
+//  mydate = new Date();
+// console.log(mydate.getTime());
+// console.log(mydate.getDay());
+// console.log(mydate.getTime());
+// console.log(mydate.getTime());
+// console.log(mydate.getTime());
+
